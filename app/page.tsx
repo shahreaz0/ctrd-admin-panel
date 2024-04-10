e04ed77cada0b10a4f21a42fa868107c8d47a983
Shahreaz Bin Alam <92302555+shahreazebd@users.noreply.github.com>
2024-04-10 12:55:42 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,10 +36,10 @@ export default function Home() {
       <div className="container  flex flex-col flex-wrap  px-6 pt-24 md:flex-row md:pt-16">
         <div className="flex w-full flex-col justify-center overflow-y-hidden lg:items-start xl:w-2/5">
           <h1 className="slide-in-bottom-h1 my-4 text-center text-3xl font-bold leading-tight text-primary md:text-left md:text-5xl">
-            CTRD Admin Panel
+            CTRD
           </h1>
           <p className="slide-in-bottom-subtitle  text-center text-base leading-normal md:text-left md:text-2xl">
-            Be kind, for whenever kindness becomes part of something
+            Coming soon...
           </p>
 
           <Link
@@ -52,7 +52,7 @@ export default function Home() {
             Login
           </Link>
 
-          <p className="pb-8 text-center font-bold text-blue-400 fade-in md:text-left lg:pb-6">
+{/*           <p className="pb-8 text-center font-bold text-blue-400 fade-in md:text-left lg:pb-6">
             Download our app:
           </p>
 
@@ -67,7 +67,7 @@ export default function Home() {
               className="bounce-top-icons h-12 cursor-pointer"
               alt="play store"
             />
-          </div>
+          </div> */}
         </div>
 
         <div className="w-full overflow-y-hidden py-6 xl:w-3/5">
