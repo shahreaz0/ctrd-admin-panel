06addcd4ffbc40f9cc3995a903f8861903db349a
shahreaz0 <ashahreaz@gmail.com>
2024-04-18 22:17:09 +0600

    style: homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import appStore from "@/public/home/app-store.svg";
-import devices from "@/public/home/devices.svg";
-import playStore from "@/public/home/play-store.svg";
+// import appStore from "@/public/home/app-store.svg";
+import comingSoon from "@/public/home/coming-soon.png";
+// import playStore from "@/public/home/play-store.svg";
 import logo from "@/public/the-citizen-trust.svg";
 
 import { cn } from "@/lib/utils";
@@ -33,13 +33,13 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="container  flex flex-col flex-wrap  px-6 pt-24 md:flex-row md:pt-16">
+      <div className="container flex flex-col flex-wrap  px-6 md:flex-row ">
         <div className="flex w-full flex-col justify-center overflow-y-hidden lg:items-start xl:w-2/5">
           <h1 className="slide-in-bottom-h1 my-4 text-center text-3xl font-bold leading-tight text-primary md:text-left md:text-5xl">
             CTRD Admin Panel
           </h1>
           <p className="slide-in-bottom-subtitle  text-center text-base leading-normal md:text-left md:text-2xl">
-            Be kind, for whenever kindness becomes part of something
+            Coming Soon...
           </p>
 
           <Link
@@ -52,7 +52,7 @@ export default function Home() {
             Login
           </Link>
 
-          <p className="pb-8 text-center font-bold text-blue-400 fade-in md:text-left lg:pb-6">
+          {/* <p className="pb-8 text-center font-bold text-blue-400 fade-in md:text-left lg:pb-6">
             Download our app:
           </p>
 
@@ -67,14 +67,15 @@ export default function Home() {
               className="bounce-top-icons h-12 cursor-pointer"
               alt="play store"
             />
-          </div>
+          </div> */}
         </div>
 
         <div className="w-full overflow-y-hidden py-6 xl:w-3/5">
           <Image
             className="slide-in-bottom mx-auto w-5/6 lg:mr-0"
-            src={devices}
-            alt="devices"
+            src={comingSoon}
+            // width={200}
+            alt="coming soon"
           />
         </div>
       </div>
